1838fcf85beb7ad5d14d77e7110b1284b5dbefdc
MasterX01 <aakashsaxena14@gmail.com>
2021-03-10 13:41:41 +0530

    [Akash] ADD. added UC-5 working code

--- SnakeLaader.java
+++ SnakeLaader.java
@@ -10,13 +10,10 @@ public class SnakeLadder {
 
                 //Game Simulation
                 int currPos = 0;
-                while(currPos <= WIN_POS) {
+                while(currPos != WIN_POS) {
                         int diceRoll = diceRoll();
                         //Option Check
                         currPos = optionSelect(currPos, diceRoll);
-
-                        if(currPos<0) //If current position goes below 0, reset
-                                currPos = 0;
 
                         System.out.println("The current Position of the player is: " + currPos);
                 }
@@ -38,18 +35,27 @@ public class SnakeLadder {
 		int option = (int)( (Math.random() * 10) % 3 );
 		switch(option) {
 			case NO_PLAY:
-				System.out.println("The option was to no play, hence the player will not advance");
+				System.out.println("The option was to no play, hence the player will not advance.");
                                 break;
+
                         case LADDER:
-                                System.out.println("The option came to Ladder, the player move forward: " + diceRoll + " steps");
                                 currPos += diceRoll;
+				if(currPos > WIN_POS) {
+					System.out.println("Since the current dice roll for Ladder will take the player out of " + WIN_POS + " bound, hence the player will not move forward.");
+					currPos -=diceRoll;
+				}else
+					System.out.println("The option came to Ladder, the player will move forward: " + diceRoll + " steps.");
                                 return currPos;
-				//break;
+
 			case SNAKE:
-                                System.out.println("The option came to Snake, the player will backward: " + diceRoll + " steps");
                                 currPos -= diceRoll;
+                        	if(currPos<0) { //If current position goes below 0, reset
+					System.out.println("Since the current dice roll for snake will take player below 0, the player will move to start position.");
+                                	currPos = 0;
+				}else
+					System.out.println("The option came to Snake, the player will move backward: " + diceRoll + " steps.");
                                 return currPos;
-			        //break;
+
                         default:
                                 System.out.println("Some error occured, default case");
                                 break;
